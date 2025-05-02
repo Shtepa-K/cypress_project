ffe65507424b0852c6a9a9d9173792751f601733
Shtepa Katerina <shtepazp87@gmail.com>
2025-05-02 22:10:49 +0300

    Homework19 registration

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,18 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+//Cypress.Commands.add('login', (email, password) => { ... })
+ 
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('https://guest:welcome2qauto@qauto.forstudy.space');
+
+ cy.get('.header_right > .btn').contains('Sign In').click();
+  
+  // Ждём появления полей и вводим данные
+  cy.get('#signinEmail', { timeout: 10000 }).should('be.visible').type(email);
+  cy.get('#signinPassword').type(password, { sensitive: true });
+  cy.get('.modal-footer > .btn-primary').contains('Login').click();
+});
 //
 //
 // -- This is a child command --
@@ -23,3 +34,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    options.log = false; // вимкнути стандартний лог
+
+    Cypress.log({
+      $el: element,
+      name: 'type',
+      message: '*'.repeat(text.length), // маскування символів
+    });
+  }
+
+  return originalFn(element, text, options);
+});
